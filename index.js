a9e0098b16a8d62cd93e54f93396c976a5fbcc08
Marat <amr404@bk.ru>
2024-05-01 22:20:36 +0300

    fix some bugs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const http = require('http');
+const path = require('path');
+const url = require('url');
 
+http.createServer((req, res) => {
+
+    let qs = require('querystring');
+    if (req.method === 'POST') {
+        let body = ''
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', async () => {
+            let post = qs.parse(body);
+            console.log(post);
+            const fetchRequest = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                },
+                body: `secret=6LeaYs0pAAAAAD1rEu5-sKK09PiKdoBvUJ6Olc9T&response=${post['g-recaptcha-response']}`
+            });
+            const data = await fetchRequest.json();
+            console.log(data);
+        });
+    }
+}).listen(3000);
